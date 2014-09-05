0d05b58b56127da4ca70ca47006c7dbbad8aae99
Tylor Janzen <tylor.janzen@gmail.com>
2014-09-05 03:23:08 -0600

    OnDemand: Add 2 step OnDemand governor

--- arch/arm/mach-msm/board-msm8x60_celox.c
+++ arch/arm/mach-msm/board-msm8x60_celox.c
@@ -271,6 +271,10 @@
 static struct platform_device ion_dev;
 #endif
 
+#ifdef CONFIG_CPU_FREQ_GOV_ONDEMAND_2_PHASE
+int set_two_phase_freq(int cpufreq);
+#endif
+
 #if defined (CONFIG_OPTICAL_GP2A) || defined(CONFIG_OPTICAL_TAOS)
 #define SENSOR_ALS_SCL   		139
 #define SENSOR_ALS_SDA   		138
@@ -16423,6 +16427,10 @@ static void __init msm8x60_init(struct msm_board_data *board_data)
 			machine_is_msm8x60_fluid() ||
 			machine_is_msm8x60_dragon())
 		msm8x60_init_ebi2();
+
+#ifdef CONFIG_CPU_FREQ_GOV_ONDEMAND_2_PHASE
+	set_two_phase_freq(1080000);
+#endif
 	msm8x60_init_tlmm();
 #ifdef CONFIG_BATTERY_SEC
 	if(is_lpm_boot)

409de7bfe0a11bffa85fa8889afffa9f903e45c8
Tylor Janzen <tylor.janzen@gmail.com>
2014-09-08 01:59:54 -0600

    Revert "MDPColorEnhance: Fix complilation"
    This reverts commit 5ebd4d6ee22739482e7287119c3f0e67cc08c5ff.

--- drivers/video/msm/mdp.c
+++ drivers/video/msm/mdp.c
@@ -44,6 +44,7 @@
 #endif
 #include "mipi_dsi.h"
 #include <linux/fcntl.h>
+#include <linux/stdlib.h>
 
 uint32 mdp4_extn_disp;
 
@@ -2265,9 +2266,7 @@ static int ColorEnhanceRead()
 {
     char path[VALUE_MAX];
     char value_str[2];
-    int enabled = 0;
     int fd;
-    int ret;
 
     snprintf(path, VALUE_MAX, "/sys/class/lcd/panel/color_enhance", "");
     fd = open(path, O_RDONLY);
@@ -2285,12 +2284,7 @@ static int ColorEnhanceRead()
 
     close(fd);
 
-    //atoi(value_str, 10, enabled);
-    ret = kstrtoint(buf, 10, &enabled);
-	if (ret < 0)
-		return(ret);
-    
-    return(enabled);
+    return(atoi(value_str));
 }
 
 static int ColorEnhanceWrite()

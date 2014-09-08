99086faeb7782cb86f2cf14ee559a6574136b20c
Tylor Janzen <tylor.janzen@gmail.com>
2014-09-08 01:59:59 -0600

    Revert "MDPColorEnhance: Fix for sysfs"
    This reverts commit 7dcb93f46ecb87c4ceb86692da52b86d916befd2.

--- arch/arm/mach-msm/board-msm8x60_celox.c
+++ arch/arm/mach-msm/board-msm8x60_celox.c
@@ -15933,59 +15933,31 @@ static ssize_t color_enhance_store(struct device *dev, struct device_attribute *
 	return size;
 }
 
-static DEVICE_ATTR(color_enhance_enabled, 0666, color_enhance_show, color_enhance_store);
+static DEVICE_ATTR(color_enhance, S_IRUGO | S_IWUGO, color_enhance_show, color_enhance_store);
 
-int color_enhance_open(struct inode *inode, struct file *filp)
-{
-	return 0;
-}
+static struct attribute *color_enhance_attributes[] = {
+	&dev_attr_color_enhance.attr,
+	NULL
+};
 
-ssize_t color_enhance_read(struct file * filp, char *buf, size_t count, loff_t * f_pos)
-{
-	char data[3] = { 0, };
-
-	get_touchkey_firmware(data);
-	put_user(data[1], buf);
-
-	return 1;
-}
-
-int color_enhance_release(struct inode *inode, struct file *filp)
-{
-	return 0;
-}
-
-struct file_operations color_enhance_fops = {
-	.owner = THIS_MODULE,
-	.read = color_enhance_read,
-	.open = color_enhance_open,
-	.release = color_enhance_release,
+static struct attribute_group color_enhance_group = {
+	.attrs = color_enhance_attributes,
 };
 
 static struct miscdevice color_enhance_device = {
 	.minor = MISC_DYNAMIC_MINOR,
 	.name = "color_enhance",
-    .fops = &color_enhance_fops,
 };
 
-static int color_enhance_device_init(void)
+static int __init color_enhance_device_init(void)
 {
-    int ret = 0;
-    
-    ret = misc_register(&color_enhance_device);
-	if (ret) {
-		printk("%s misc_register fail\n", __FUNCTION__);
-        goto fail;
+	misc_register(&color_enhance_device);
+	if (sysfs_create_group(&color_enhance_device.this_device->kobj, &color_enhance_group) < 0)
+	{
+		printk("%s sysfs_create_group fail\n", __FUNCTION__);
+		pr_err("Failed to create sysfs group for device (%s)!\n", color_enhance_device.name);
 	}
-    
-    ret = device_create_file(color_enhance_device.this_device, &dev_attr_color_enhance_enabled);
-	if (ret < 0)
-		printk("%s device_create_file fail\n", __FUNCTION__);
-		pr_err("Failed to create sysfs for device (%s)!\n", dev_attr_color_enhance_enabled.attr.name);
-        goto fail;
-        
-fail:
-    return ret;
+	return 0;
 }
 
 #if defined(CONFIG_FB_MSM_MIPI_S6E8AA0_HD720_PANEL)
